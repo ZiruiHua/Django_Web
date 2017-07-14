797dfe47978010fd543e3111604120c7ca4881ae
ziruih <hua940201@gmail.com>
2017-07-14 19:13:30 +0800

    Removed folder from repository

--- Django_test/webTest/webapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    #url(r'^$', views.index, name='index'),
-    url(r'^pass/', views.index, name='valuepass'),
-    url(r'^showresult/', views.valuePass, name='result'),
-]
